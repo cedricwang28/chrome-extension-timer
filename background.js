e2ab9727a77a172ae7a0b77eb45f4cbe8e10135b
Cedricwang <464231692@qq.com>
2023-02-06 02:16:07 -0700

    initial

---
+++ background.js
@@ -0,0 +1,36 @@
+chrome.alarms.create("Timer", {
+  periodInMinutes: 1 / 60,
+});
+
+chrome.alarms.onAlarm.addListener((alarm) => {
+  if (alarm.name === "Timer") {
+    chrome.storage.local.get(["timer", "isRunning", "timeOption"], (res) => {
+      if (res.isRunning) {
+        let timer = res.timer + 1;
+        console.log(timer);
+        let isRunning = true;
+        if (timer === 60 * res.timeOption) {
+          console.log(this, "time is up");
+          this.registration.showNotification("timer", {
+            body: `${res.timeOption} minites has passed!`,
+            icon: "icon.png",
+          });
+          timer = 0;
+          isRunning = false;
+        }
+        chrome.storage.local.set({
+          timer,
+          isRunning,
+        });
+      }
+    });
+  }
+});
+
+chrome.storage.local.get(["timer", "isRunning", "timeOption"], (res) => {
+  chrome.storage.local.set({
+    timer: "timer" in res ? res.timer : 0,
+    timeOption: "timeOption" in res ? res.timeOption : 25,
+    isRunning: "isRunning" in res ? res.isRunning : false,
+  });
+});
